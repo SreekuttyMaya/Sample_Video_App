b0aa66e7f279772dab056804c3b852395be947af
Sreekutty <sreekutty@3eplc.com>
2018-04-25 14:54:18 +0530

    https://github.com/SreekuttyMaya/Sample_Video_App.git

---
+++ Video_Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Video_Application
+//
+//  Created by Sreekutty on 25/04/18.
+//  Copyright © 2018 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Video_ApplicationTests/Video_ApplicationTests.m
@@ -0,0 +1,39 @@
+//
+//  Video_ApplicationTests.m
+//  Video_ApplicationTests
+//
+//  Created by Sreekutty on 25/04/18.
+//  Copyright © 2018 abc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Video_ApplicationTests : XCTestCase
+
+@end
+
+@implementation Video_ApplicationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
