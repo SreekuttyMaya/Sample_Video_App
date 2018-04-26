1e2c2d1d7836213d555a93d023de7466447a4338
Sreekutty <sreekutty@3eplc.com>
2018-04-26 16:09:18 +0530

    https://github.com/SreekuttyMaya/Sample_Video_App.git

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Video_Application' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for Video_Application
+  pod 'SDWebImage'
+
+  target 'Video_ApplicationTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Video_Application/MVC/Controller/Home/HomeViewController.m
+++ Video_Application/MVC/Controller/Home/HomeViewController.m
@@ -7,9 +7,19 @@
 //
 
 #import "HomeViewController.h"
+#import "UIImageView+WebCache.h"
+#import <UIView+WebCache.h>
+#import <AVKit/AVKit.h>
 
 @interface HomeViewController ()
-
+{
+    
+    __weak IBOutlet UICollectionView *collectionView_images;
+    __weak IBOutlet UIView *view_video;
+    __weak IBOutlet UIScrollView *scrollView;
+    
+    NSMutableArray *array_images;
+}
 @end
 
 @implementation HomeViewController
@@ -17,6 +27,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    //load image to load in carousel
+    array_images = [NSMutableArray new];
+    for (int i = 0; i < 7; i++) {
+        [array_images addObject:[NSString stringWithFormat:@"https://picsum.photos/%d/%d",(i+1)*100,(i+2)*100]];
+    }
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self playVideoSetUp];
+    
+}
+
+-(void)playVideoSetUp {
+    
+    NSString *urlString=[NSString stringWithFormat:@"%@",@"http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8"];
+    AVAsset *asset = [AVAsset assetWithURL:[NSURL URLWithString:urlString]];
+    AVPlayerItem *avPlayerItem = [[AVPlayerItem alloc]initWithAsset:asset];
+    AVPlayer *songPlayer = [AVPlayer playerWithPlayerItem:avPlayerItem];
+    AVPlayerLayer *avPlayerLayer = [AVPlayerLayer playerLayerWithPlayer: songPlayer];
+    avPlayerLayer.frame = view_video.layer.bounds;
+    songPlayer.muted = YES;
+    [view_video.layer addSublayer:avPlayerLayer];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [songPlayer play];
+    });
 }
 
 - (void)didReceiveMemoryWarning {
